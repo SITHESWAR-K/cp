531fcb1ab163b1a094dd9eb60b8411b7c4b023f0
sithes1011 <sithes1011@gmail.com>
2025-12-14 19:35:17 +0530

    first commit

---
+++ hi.java
@@ -0,0 +1,14 @@
+import java.awt.SystemColor;
+import java.util.Scanner;
+class hello{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        scan.nextLine();
+        int mark = scan.nextInt();
+        String dept = scan.nextLine();
+        System.out.println(name);
+        System.out.println(mark*0.1+);
+    }
+
+}
